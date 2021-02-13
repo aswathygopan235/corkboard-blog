76ff657434d8bcd27e996bde1450b2f29112ebeb
Aswathy Gopan <aswathygopan0523@gmail.com>
2021-02-13 15:20:29 +0530

    navbar done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,35 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Corkboard</title>
 </head>
-<body>
-    @yield('content')
+<body class='bg-gray-100'>
+    <nav class="p-6 bg-white flex justify-between">
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Home</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Dashboard</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Posts</a>
+            </li>
+        </ul>
+        <!--User details-->
+        <ul class="flex items-center">
+            <li>
+                <a href="" class="p-3">Name</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Login</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Register</a>
+            </li>
+            <li>
+                <a href="" class="p-3">Logout</a>
+            </li>
+        </ul>
+    </nav>
+        @yield('content')
 </body>
 </html>
