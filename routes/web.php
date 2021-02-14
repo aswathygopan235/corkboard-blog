c9d2ac034130fa947c064587f26eaf802c5f4c6f
Aswathy Gopan <aswathygopan0523@gmail.com>
2021-02-14 22:04:18 +0530

    Show single post page

--- routes/web.php
+++ routes/web.php
@@ -64,6 +64,8 @@ Route::post('/logout', [LogoutController::class,'store'])->name('logout');
  */
 Route::get('/posts', [PostController::class,'index'])->name('posts');
 Route::post('/posts', [PostController::class,'store']);
+Route::get('/posts/{post}', [PostController::class,'show'])->name('posts.show');
+
 Route::delete('/posts/{post}', [PostController::class,'destroy'])->name('posts.destroy');
 
 Route::post('/posts/{post}/likes', [PostLikeController::class,'store'])->name('posts.likes');
